4438d90509f0073be0b799a6edf739382140b41b
eduardodiaz777 <l25170881@culiacan.tecnm.mx>
2026-03-14 19:43:27 +0000

    intento 1

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -12,15 +12,23 @@ public class Principal {
 
         // Solicitar los datos en el siguiente orden:
         // descripcion, peso en kilos y precio por kilo
-
+        System.out.print("Proporcione la descripcion del producto:");
+        String descripcion = entrada.nextLine();
+        System.out.print("Proporcione el peso en kilos:");
+        double peso = entrada.nextDouble();
+        
+        System.out.print("Proporcione el precio por kilo:");
+        double precio = entrada.nextDouble();
         // Crear un objeto Producto con los datos ingresados
-
+        Producto producto = new Producto(descripcion, peso, precio);
         // Linea en blanco
         System.out.println();
 
         // Mostrar los datos del producto
-
+        System.out.println(producto);
         // Mostrar el precio del producto
+        System.out.printf("Precio del producto: $%,.2f\n",
+        producto.calcularPrecioProducto());
 
         entrada.close();
     }
--- miPrincipal/Producto.java
+++ miPrincipal/Producto.java
@@ -11,42 +11,52 @@ public class Producto {
     // Constructor
     public Producto(String descripcion, double pesoKilo, double precioKilo) {
         // Mandar llamar a los metodos set de los atributos
+        setDescripcion(descripcion);
+        setPesoKilo(pesoKilo);
+        setPrecioKilo(precioKilo);
     }
 
     // Metodos set/get
     public void setDescripcion(String descripcion) {
         // La descripción no se valida
+        this.descripcion=descripcion;
+
     }
 
     public void setPesoKilo(double pesoKilo) {
         // El peso en kilos debe ser mayor o igual a 0
+        if(pesoKilo>=0)
+            this.pesoKilo=pesoKilo;
     }
 
     public void setPrecioKilo(double precioKilo) {
         // El precio por kilo debe ser mayor o igual a 0
+         if(precioKilo>=0)
+            this.precioKilo=precioKilo;
     }
 
     public String getDescripcion() {
-        return "";
+        return descripcion;
     }
 
     public double getPesoKilo() {
-        return 0.0;
+        return pesoKilo;
     }
 
     public double getPrecioKilo() {
-        return 0.0;
+        return precioKilo;
     }
 
     // Metodo para calcular el precio total del producto
     public double calcularPrecioProducto() {
-        return 0.0;
+        return pesoKilo*precioKilo;
+
     }
 
     // Sobrescribir el metodo toString para mostrar la informacion del producto
     @Override
     public String toString() {
-        String resultado = String.format("Producto: %s, Peso: %.2f kg, Precio por kilo: $%,.2f",
+        String resultado = String.format("  Producto: %s, Peso: %.2f kg, Precio por kilo: $%,.2f",
                 descripcion, pesoKilo, precioKilo);
         return resultado;
     }
